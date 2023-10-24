40629cbab7fa39b231699f21670a45203ddce60f
CatiaBio <s213049@dtu.dk>
2023-10-24 10:23:58 +0200

    q

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Translation
+#'
+#' A table with RNA codons and respective amino acids
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"table_codons"
