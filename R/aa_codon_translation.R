e588bbcf0fd52db5c7cc41b893754a9f47db094a
EiriniGian <s230204@dtu.dk>
2023-10-24 11:25:05 +0200

    Everything.

---
+++ R/aa_codon_translation.R
@@ -0,0 +1,14 @@
+#' amino acids to codons translation
+#'
+#' @param aa_seq original sequence of amino acids
+#' @param start first position of the input sequence
+#'
+#' @return codons sequence corresponding to the aa original sequence
+#' @export
+aa_codon_translation <- function(aa_seq, start = 1){
+  seq_length <- nchar(aa_seq)
+  codons <- substring(aa_seq,
+                      first = seq(from = start, to = seq_length-3+1, by = 3),
+                      last = seq(from = 3+start-1, to = seq_length, by = 3))
+  return(codons)
+}
---
+++ R/transcription.R
@@ -0,0 +1,11 @@
+#' Transcription
+#'
+#' @param DNA_sequence a DNA sequence to be transcribed
+#'
+#' @return Returns a RNA sequence of the inserted DNA sequence
+#' @export
+#'
+transcription <- function(DNA_sequence){
+  RNA_sequence <- gsub("T", "U", DNA_sequence)
+  return(RNA_sequence)
+}
