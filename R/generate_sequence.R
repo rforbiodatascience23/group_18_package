6768b2f69322ad4fc4f1107f751a8663cdc1458e
MI00s <s222854@dtu.dk>
2023-10-24 08:49:01 +0000

    Function 1.

---
+++ R/generate_sequence.R
@@ -0,0 +1,12 @@
+#' Random sequence
+#'
+#' @param number_nucleotides
+#'
+#' @return the function will return sequence corresponding with the number of nucleotides
+#' @export
+#'
+generated_sequence <- function(number_nucleotides){
+  selected_sequence <- sample(c("A", "T", "G", "C"), size = number_nucleotides, replace = TRUE)
+  clean_sequence <- paste0(selected_sequence, collapse = "")
+  return(clean_sequence)
+}
