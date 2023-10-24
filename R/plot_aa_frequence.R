a9b56b3bfa698534dbdccd95313ac164e0bc0716
CatiaBio <s213049@dtu.dk>
2023-10-24 14:46:39 +0200

    working funtion 5

---
+++ R/plot_aa_frequence.R
@@ -0,0 +1,27 @@
+#' Frequency of amino acids
+#'
+#' @param amino_acid_sequence
+#'
+#' @return amino_acid_frequency_plot a plot with the amino acid counts of a given amino acid sequence
+#' @export
+#'
+plot_aa_frequence <- function(amino_acid_sequence){
+  amino_acid <- amino_acid_sequence |>
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
+    as.character() |>
+    unique()
+
+  counts <- sapply(amino_acid, function(amino_acid) stringr::str_count(string = amino_acid_sequence, pattern =  amino_acid)) |>
+    as.data.frame()
+
+  colnames(counts) <- c("Counts")
+  counts[["Amino_Acids"]] <- rownames(counts)
+
+  amino_acid_frequency_plot <- counts |>
+    ggplot2::ggplot(ggplot2::aes(x = Amino_Acids, y = Counts, fill = Amino_Acids)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+
+  return(amino_acid_frequency_plot)
+}
